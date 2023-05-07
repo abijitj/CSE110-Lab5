044dab84670c46e5647728960c8ab45f698aac1e
abijitj <abijit.jay@gmail.com>
2023-05-07 16:06:08 -0700

    Fixed list not populating bug - added delay

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,19 +4,25 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   let voices = speechSynthesis.getVoices(); 
-  console.log(voices); 
+  //console.log(voices); 
   let voiceSelect = document.getElementById('voice-select'); 
   console.log(voiceSelect); 
 
-  for(let i = 0; i < voices.length; i++){
-    const option = document.createElement('option'); 
+  setTimeout(populateList, 100); 
 
-    option.textContent = `${voices[i].name} (${voices[i].lang})`;
-
-    option.setAttribute("data-lang", voices[i].lang);
-    option.setAttribute("data-name", voices[i].name);
-    voiceSelect.appendChild(option)    
+  function populateList(){
+    console.log(voices);
+    for(let i = 0; i < voices.length; i++){
+      const option = document.createElement('option'); 
+  
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option)    
+    }
   }
+
 
   let readTxt = document.getElementById('text-to-speak'); 
   let faceImg = document.querySelector('img'); 
@@ -27,29 +33,13 @@ function init() {
   speakBtn.addEventListener("click", function(){
     let utterThis = new SpeechSynthesisUtterance(readTxt.value);
     console.log(readTxt.value); 
-    //let currOption = document.getElementsByClassName()
     utterThis.voice = voices[voiceSelect.selectedIndex - 1];
     console.log(voiceSelect.selectedIndex); 
-    //utterThis.lang = utterThis.voice.lang;
     speechSynthesis.speak(utterThis);
     faceImg.src = 'assets/images/smiling-open.png' 
 
     utterThis.addEventListener("end", function(){
       faceImg.src = 'assets/images/smiling.png'
     })
-    //speakingMouth(); 
   })
-
-
-
-
-
-  function speakingMouth(){
-    while(speechSynthesis.speaking == true){
-      console.log("In the loop...");
-    }
-    faceImg.src = 'assets/images/smiling.png';
-  }
-
-
 }
